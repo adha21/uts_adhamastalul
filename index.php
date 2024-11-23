eeb9b744776e2e753076103ea1ab9ad6e7523b06
adha21 <adhamastalulala@gmail.com>
2024-11-23 10:14:59 +0700

    menambah css dan js

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Tracker</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+include_once('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+            <div class="card-header text-center">
+                <h3>Order Tracker</h3>
+            </div>
+            <div class="card-body">
+                <h4>Halo, Selamat Datang </h4>
+                <p>Track my package has never been that easy with this global parcel tracking tool allowing you to track parcels from any post office. A powerful universal parcel tracking system that will provide you with  shipment info by tracking number lookup from any international courier such as China Post, Speedpak, 4PX Express, Amazon Logistics TBA, EMS ePacket, Dynamex, Aliexpress - Standard Shipping, Intelcom Express, Hermes, Deutsche Post - DHL Global mail - DHL Ecommerce, Yanwen, Canpar, Yunexpress, Pitney Bowes Inc - PBI, BNI - BNICA and many more!</p>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
